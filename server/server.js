c63280cf7969b73425643847a91fe4e8f6baca33
Hisham C <hishamc18@gmail.com>
2025-06-27 22:04:51 +0530

    completed and hosted

--- server/server.js
+++ server/server.js
@@ -11,8 +11,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true 
+}));
 
-app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
