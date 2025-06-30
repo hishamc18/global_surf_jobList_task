1b95296ccc7de50c17de897058697d64f3aaace9
Hisham C <hishamc18@gmail.com>
2025-06-30 10:43:32 +0530

    env adjusted

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: '/api',
-  baseURL: 'http://localhost:3000/api',
+  baseURL: '/api',
+  // baseURL: 'http://localhost:3000/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
